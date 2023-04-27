8a66de918c8f6a0182c6992031d1bf441d557b50
Soul <sulaimanalyousefi2003@gmail.com>
2023-04-27 15:18:09 +0400

    finished without tests

--- pa3_133607.py
+++ pa3_133607.py
@@ -1,15 +1,102 @@
 """
 Name:Sulaiman Khalifa Khalfan Al Yousfi
 ID:s133607
-Lab:
-Exe:
-Purpose:
+HW:3
+Purpose: Write a program that adds,display statics of invoice , remove an order.
 Input:
+    Menu:
+        1) Add order
+        2) Display order
+        3) Display all orders
+        4) Display orders per customer
+        5) Display orders per country
+        6) Display orders statistics
+        7) Remove order
+        8) Exit program
+    Order:
+        1)Phone number.
+        2)Destination.
+        3)order weight.
 Output:
+    Adding an order:displays the entered data in a table.
+    display order:displays the entered data in a table using the given id.
+    Display all order: display all data in table form.
+    Display orders per customer:displays all the orders for the inputted number.
+    Display orders per country:displays all the orders for the inputted country.
+    Display orders statistics:displays: number of orders, total cost of all orders, average orders cost,order with
+                              highest and lows cost.
 Algorithm:
+    Main:
+        1)Create the needed lists.
+        2)Loop till the user chooses exit option
+            1) if 1 then uses addOrder function
+            2) if 8 then prompt the user a validation msg for exiting.
+                1)if yes then exit
+                2)if no then continue.
+                3)else error msg.
+            3) if user entered from (2 - 7) without adding any orders first print error msg.
+            4) if 2 then use displayOrder.
+            5) if 3 then use displayAllOrder.
+            6) if 4 then use displayCustomersOrder.
+            7) if 5 then use displayCountryOrder.
+            8) if 6 then use displayStatistics.
+            9) if 7 then use removeOrder.
+            10) else print error.
+    DisplayMenu:
+        1)Display menu.
+        2)returns the user choice.
+    AddOrder:
+        1)Create a list of countries.
+        2)Prompt the user for input
+        3)Validate all the inputs, else print the proper error msg.
+        4)Calculate the cost according the region.
+        5)Generate a random number that is in a specific range and that doesn't exist in the id list.
+        6)Append the data to their appropriate lists.
+    DisplayOrder:
+        1)Prompt the user for the id.
+        2)Check weather the input is in the right format and if it in the list, else prints the appropriate msg.
+        3)Get the index of the id.
+        4)Print a table of the data of the index.
+    DisplayAllOrders:
+        1)Prints the header of the table.
+        2)Loops for the length of any list and prints the data formatted.
+        3)Print the bottom table line.
+    DisplayCustomersOrders:
+        1)Prompts the user for the phone number.
+        2)Checks weather the phone number is in right format and if it's in the phone number list,else print the
+          appropriate msg.
+        3)Print the header.
+        4)Loop the length of the list.
+            1)if the input matches the index value of the phone list, print data of the index.
+        5)Print the bottom line of the table
+    DisplayCountryOrders:
+        1)Prompts the user for input.
+        2)check if the country is in the gcclist, else print error msg
+        3)Print Header.
+        4)loop for length of list
+            1)if the country matches the index value of the destination list, print data of the index.
+        5)Print the bottom line of the table
+    DisplayStatistics:
+        1)Get the number of orders ising the len function of on one of the lists.
+        2)Get the total cost use sum function on the orderCost function.
+        3)Calculate the average.
+        4)get the max cost using the max function orderCost list.
+        5)Get the index of max cost.
+        6)get the mix cost using the min function orderCost list.
+        7)Get the index of min cost.
+        8)Print order number , total cost, average.
+        9)Print the max order data.
+        10)Print the min order data.
+    RemoveOrder:
+        1)Prompt the user for input.
+        2)Check weather the input is digit and if it's in the id list, else print the appropriate error msg.
+        3)Get the index of the input id.
+        4)remove the index of all the lists using pop.
+        5)Print conformation of removal.
 Test:
 """
 from random import randint
+from sys import exit
 
 
 def main():
@@ -23,21 +110,7 @@ def main():
         choice = displayMenu()
         if choice == '1':
             addOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
-        elif  choice in ["2", "3", "4", "5", "6", "7", "8"] and len(customerPhone) == 0 :
-            print("No orders yet recorded. Select 1) Add order first!")
-        elif choice == '2' :
-            displayOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
-        elif choice == '3' :
-            displayAllOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
-        elif choice == '4' :
-            displayCustomer(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
-        elif choice == '5' :
-            displayCountryOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
-        elif choice == '6' :
-            displayStatistics(customerPhone, orderNumber, destinationCountry, orderCost)
-        elif choice == '7' :
-            removeOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
-        elif choice == '8' :
+        elif choice == '8':
             end = input("Are you sure you want to exit the application y/n: ")
             if end == "y":
                 exit("Goodbye!")
@@ -45,23 +118,34 @@ def main():
                 continue
             else:
                 print("ERROR: Invalid choice! Try again.")
+        elif choice in ["2", "3", "4", "5", "6", "7"] and len(customerPhone) == 0:
+            print("No orders yet recorded. Select 1) Add order first!")
+        elif choice == '2':
+            displayOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
+        elif choice == '3':
+            displayAllOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
+        elif choice == '4':
+            displayCustomer(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
+        elif choice == '5':
+            displayCountryOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
+        elif choice == '6':
+            displayStatistics(customerPhone, orderNumber, destinationCountry, orderCost)
+        elif choice == '7':
+            removeOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost)
 
         else:
             print("ERROR: Invalid choice! Try again.")
 
 
-
-
-
-
-
 def displayMenu():
-    print('****************************************\n ', '		 Al-Yaqeen Logistics\n ', '****************************************\n ', 'Select an operation:\n ', '1) Add order\n ', '2) Display order\n ', '3) Display all orders\n ',
-          '4) Display orders per customer\n ', '5) Display orders per country\n ', '6) Display orders statistics\n ', '7) Remove order\n ', '8) Exit program\n ', '****************************************')
+    print('****************************************\n ', '		 Al-Yaqeen Logistics\n ',
+          '****************************************\n ', 'Select an operation:\n ', '1) Add order\n ',
+          '2) Display order\n ', '3) Display all orders\n ',
+          '4) Display orders per customer\n ', '5) Display orders per country\n ', '6) Display orders statistics\n ',
+          '7) Remove order\n ', '8) Exit program\n ', '****************************************')
 
     choice = input("Enter your choice: ")
-    if choice in ["1", "2", "3", "4", "5", "6", "7", "8"]:
-        return choice
+    return choice
 
 
 def addOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost):
@@ -72,11 +156,13 @@ def addOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderC
         if len(tempList) == 3:
             if len(tempList[0]) == 8 and tempList[0].isdigit():
                 if tempList[1].lower() in gccList:
-                    if (tempList[2].isdigit() or (tempList[2].count('.') == 1 and tempList[2].replace('.', '').isdigit())) and float(tempList[2]) > 0:
+                    if (tempList[2].isdigit() or (
+                            tempList[2].count('.') == 1 and tempList[2].replace('.', '').isdigit())) and float(
+                                tempList[2]) > 0:
                         phoneNo = int(tempList[0])
                         dest = tempList[1]
                         weight = float(tempList[2])
-                        cost = 0
+
                         if dest == "oman":
                             cost = weight * 0.950
                         elif dest in ["emirates", "uae", "qatar"]:
@@ -93,7 +179,8 @@ def addOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderC
                         orderCost.append(cost)
                         print("A new order has been added...")
                         print("*" * 75)
-                        print("%-16s%-16s%-16s%-16s%-10s" % ("Customer Phone", "Order Number", "Country", "Weight", "Cost"))
+                        print("%-16s%-16s%-16s%-16s%-10s" % (
+                            "Customer Phone", "Order Number", "Country", "Weight", "Cost"))
                         print("*" * 75)
                         print("%-16d%-16d%-16s%-16.2f%-10.3f" % (phoneNo, random, dest.upper(), weight, cost))
                         print("*" * 75)
@@ -101,7 +188,8 @@ def addOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderC
                     else:
                         print("ERROR: Invalid weight! Try again.")
                 else:
-                    print("ERROR: None GCC country:", tempList[1], "\nCurrently no shipping services outside GCC.\nTry again.")
+                    print("ERROR: None GCC country:", tempList[1],
+                          "\nCurrently no shipping services outside GCC.\nTry again.")
             else:
                 print("ERROR: Invalid phone number. Try again!")
         elif len(tempList) > 3:
@@ -117,7 +205,9 @@ def displayOrder(customerPhone, orderNumber, destinationCountry, orderWeight, or
         print("*" * 75)
         print("%-16s%-16s%-16s%-16s%-10s" % ("Customer Phone", "Order Number", "Country", "Weight", "Cost"))
         print("*" * 75)
-        print("%-16d%-16d%-16s%-16.2f%-103f" % (customerPhone[index], orderNumber[index], destinationCountry[index].upper(), orderWeight[index], orderCost[index]))
+        print("%-16d%-16d%-16s%-16.2f%-10.3f" % (
+            customerPhone[index], orderNumber[index], destinationCountry[index].upper(), orderWeight[index],
+            orderCost[index]))
         print("*" * 75)
     else:
         print("ERROR: Invalid order number! Try again.")
@@ -128,14 +218,15 @@ def displayAllOrder(customerPhone, orderNumber, destinationCountry, orderWeight,
     print("%-16s%-16s%-16s%-16s%-10s" % ("Customer Phone", "Order Number", "Country", "Weight", "Cost"))
     print("*" * 75)
     for i in range(len(customerPhone)):
-        print("%-16d%-16d%-16s%-16.2f%-10.3f" % (customerPhone[i], orderNumber[i], destinationCountry[i].upper(), orderWeight[i], orderCost[i]))
+        print("%-16d%-16d%-16s%-16.2f%-10.3f" % (
+            customerPhone[i], orderNumber[i], destinationCountry[i].upper(), orderWeight[i], orderCost[i]))
     print("*" * 75)
 
 
 def displayCustomer(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost):
     num = input("Enter customer phone: ")
     if num.isdigit() and len(num) == 8:
-        if num in customerPhone:
+        if int(num) in customerPhone:
             total = 0.0
             print("*" * 75)
             print("%-16s%-16s%-16s%-16s%-10s" % ("Customer Phone", "Order Number", "Country", "Weight", "Cost"))
@@ -143,7 +234,8 @@ def displayCustomer(customerPhone, orderNumber, destinationCountry, orderWeight,
             for i in range(len(customerPhone)):
                 if customerPhone[i] == int(num):
                     total += orderCost[i]
-                    print("%-16d%-16d%-16s%-16.2f%-10.3f" % (customerPhone[i], orderNumber[i], destinationCountry[i].upper(), orderWeight[i], orderCost[i]))
+                    print("%-16d%-16d%-16s%-16.2f%-10.3f" % (
+                        customerPhone[i], orderNumber[i], destinationCountry[i].upper(), orderWeight[i], orderCost[i]))
             print("*" * 75)
             print("Total cost of orders made by ", num, " is OMR", total, ".")
         else:
@@ -163,7 +255,8 @@ def displayCountryOrder(customerPhone, orderNumber, destinationCountry, orderWei
         for i in range(len(destinationCountry)):
             if destinationCountry[i] == name.lower():
                 total += orderCost[i]
-                print("%-16d%-16d%-16s%-16.2f%-10.3f" % (customerPhone[i], orderNumber[i], destinationCountry[i].upper(), orderWeight[i], orderCost[i]))
+                print("%-16d%-16d%-16s%-16.2f%-10.3f" % (
+                    customerPhone[i], orderNumber[i], destinationCountry[i].upper(), orderWeight[i], orderCost[i]))
         print("*" * 75)
         print("Total cost of orders shipped to ", name, " is OMR", total, ".")
     else:
@@ -183,18 +276,22 @@ def displayStatistics(customerPhone, orderNumber, destinationCountry, orderCost)
     print("-" * 80)
     print("Total orders cost: ", totalOrders)
     print("-" * 80)
+    print("Average order cost: ", avg)
+    print("-" * 80)
     print("Order with highest cost:")
-    print("orders number: ", orderNumber[maxPos], ", customer phone: ", customerPhone[maxPos], ", country: ", destinationCountry[maxPos], ", cost: ", orderCost[maxPos])
+    print("orders number: ", orderNumber[maxPos], ", customer phone: ", customerPhone[maxPos], ", country: ",
+          destinationCountry[maxPos], ", cost: ", orderCost[maxPos])
     print("-" * 80)
     print("Order with lowest cost:")
-    print("orders number: ", orderNumber[minPos], ", customer phone: ", customerPhone[minPos], ", country: ", destinationCountry[minPos], ", cost: ", orderCost[minPos])
+    print("orders number: ", orderNumber[minPos], ", customer phone: ", customerPhone[minPos], ", country: ",
+          destinationCountry[minPos], ", cost: ", orderCost[minPos])
     print("*" * 80)
 
 
 def removeOrder(customerPhone, orderNumber, destinationCountry, orderWeight, orderCost):
     num = input("Enter order number: ")
-    if num.isdigit() and num in orderNumber:
-        pos = orderCost.index(int(num))
+    if num.isdigit() and int(num) in orderNumber:
+        pos = orderNumber.index(int(num))
         customerPhone.pop(pos)
         orderNumber.pop(pos)
         destinationCountry.pop(pos)
